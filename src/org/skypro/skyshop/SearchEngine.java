b3778b3a1dd883eddb78c832ff4e893bca627a43
Mimiko2207 <loparev.m@mail.ru>
2025-09-19 17:17:47 +0700

    OOP3 (полиморфизм)

--- src/org/skypro/skyshop/SearchEngine.java
+++ src/org/skypro/skyshop/SearchEngine.java
@@ -4,9 +4,42 @@ import org.skypro.skyshop.product.org.skypro.skyshop.basket.Article;
 import org.skypro.skyshop.product.org.skypro.skyshop.basket.Searchable;
 
 public class SearchEngine {
+
+    private Searchable[] items;
+    private int count;
+
+
     public void add(Article product1) {
     }
 
     public Searchable[] search(String term) {
     }
-}
+    public void SearchEngine(int size) {
+        this.items = new Searchable[size];
+        this.count = 0;
+    }
+
+    public Searchable[] add(Searchable item) {
+        if (count < items.length) {
+            items[count++] = item;
+        } else {
+            // Можно добавить обработку переполнения, если нужно
+            System.out.println("Массив полон, добавление невозможно");
+
+        }
+    public Searchable[] search(String searchTerm) {
+        Searchable[] results = new Searchable[5];
+        int foundCount = 0;
+
+        for (Searchable item : items) {
+            if (item != null && item.getSearchTerm().contains(searchTerm)) {
+                results[foundCount++] = item;
+                if (foundCount == 5) {
+                    break;
+                }
+            }
+
+            return results;
+        }
+
+
--- src/org/skypro/skyshop/product/org/skypro/skyshop/basket/Searchable.java
+++ src/org/skypro/skyshop/product/org/skypro/skyshop/basket/Searchable.java
@@ -2,8 +2,6 @@ package org.skypro.skyshop.product.org.skypro.skyshop.basket;
 
 public interface Searchable {
 
-    private Searchable[] items;
-    private int count;
 
     String getSearchTerm();
 
@@ -18,34 +16,7 @@ public interface Searchable {
         return getName() + " — тип " + getContentType();
 
     }
-    public void SearchEngine(int size) {
-        this.items = new Searchable[size];
-        this.count = 0;
-    }
 
-    public void add(Searchable item) {
-        if (count < items.length) {
-            items[count++] = item;
-        } else {
-            // Можно добавить обработку переполнения, если нужно
-            System.out.println("Массив полон, добавление невозможно");
-        }
-    }
-
-    public Searchable[] search(String searchTerm) {
-        Searchable[] results = new Searchable[5];
-        int foundCount = 0;
-
-        for (Searchable item : items) {
-            if (item != null && item.getSearchTerm().contains(searchTerm)) {
-                results[foundCount++] = item;
-                if (foundCount == 5) {
-                    break;
-                }
-            }
-
-        return results;
-    }
 
     }
 
